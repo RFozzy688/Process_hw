ff9e85532fb5ceeac2f6c3c645f051efdca2bca6
RFozzy688 <pokerfozzy@gmail.com>
2023-10-24 22:11:43 +0300

    start

---
+++ Process_hw/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Process process = new Process();
+            process.StartInfo = new ProcessStartInfo("calc.exe");
+            process.Start();
+        }
+    }
+}
