bbc057b7f4c4af87d1ac55acf14d19b66a4e88d5
RFozzy688 <pokerfozzy@gmail.com>
2023-10-24 23:53:09 +0300

    finish

--- task_3/Program.cs
+++ task_3/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,11 @@ namespace task_3
     {
         static void Main(string[] args)
         {
+            Process process = new Process();
+            process.StartInfo = new ProcessStartInfo(@"..\..\..\task_3_app\bin\Debug\task_3_app.exe");
+            process.StartInfo.Arguments = "10 5 /";
+            process.Start();
+            process.WaitForExit();
         }
     }
 }
---
+++ task_3_app/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_3_app
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("task_3_app");
+
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Wrong arguments!!!");
+                Console.ReadKey();
+                return;
+            }
+
+            double result = 0;
+            char action = ' ';
+            double[] numbers = new double[2];
+            int i = 0;
+
+            foreach (var el in args)
+            {
+                if (el == "+")
+                {
+                    action = '+';
+                }
+                else if (el == "*")
+                {
+                    action = '*';
+                }
+                else if (el == "-")
+                {
+                    action = '-';
+                }
+                else if (el == "/")
+                {
+                    action = '/';
+                }
+                else
+                {
+                    numbers[i] = Double.Parse(el);
+                    i++;
+                }
+            }
+
+            switch (action)
+            {
+                case '+':
+                    result = numbers[0] + numbers[1];
+                    break;
+                case '-':
+                    result = numbers[0] - numbers[1];
+                    break;
+                case '*':
+                    result = numbers[0] * numbers[1];
+                    break;
+                case '/':
+                    result = numbers[0] / numbers[1];
+                    break;
+                default:
+                    break;
+            }
+            Console.WriteLine($"Result = {result}");
+            Console.ReadKey();
+        }
+    }
+}
