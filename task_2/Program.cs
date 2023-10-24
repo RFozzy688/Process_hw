0ec1c8b4eaf6fbaa3c139e8210c33e6e290e132d
RFozzy688 <pokerfozzy@gmail.com>
2023-10-24 22:34:39 +0300

    finish

--- task_2/Program.cs
+++ task_2/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,26 @@ namespace task_2
     {
         static void Main(string[] args)
         {
+            Process process = new Process();
+            process.StartInfo = new ProcessStartInfo("notepad.exe");
+            process.Start();
+
+            Console.WriteLine("Запущен процесс: " + process.ProcessName);
+            Console.Write("1 - ждать\n2 - завершить процесс\n > ");
+            string str = Console.ReadLine();
+
+            if (str == "1")
+            {
+                Console.WriteLine("Процесс ждет закрытия");
+                process.WaitForExit();
+            }
+            else
+            {
+                Console.WriteLine("Процесс завершен");
+                process.Kill();
+            }
+
+            Console.WriteLine("Процесс завершился с кодом: " + process.ExitCode);
         }
     }
 }
